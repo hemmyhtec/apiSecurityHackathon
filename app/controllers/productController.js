3e4fcfb497a895b7b716a4559f3119310f923186
Kolawole Emmanuel <76248385+hemmyhtec@users.noreply.github.com>
2023-07-05 13:33:32 +0100

    Update productController.js

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -1,4 +1,4 @@
-import uploadImageToCloudinary from "../../config/cloudinary.js";
+//import uploadImageToCloudinary from "../../config/cloudinary.js";
 import mongoose from "mongoose";
 import storeSchema from "../models/store.js";
 import Product from "../models/product.js";
