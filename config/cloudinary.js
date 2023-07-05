4ce65d576e267942c29cddb513b59d89a346ecde
Kolawole Emmanuel <76248385+hemmyhtec@users.noreply.github.com>
2023-07-05 13:32:36 +0100

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,26 +1,26 @@
-import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv'
-dotenv.config()
+//import { v2 as cloudinary } from 'cloudinary';
+//import dotenv from 'dotenv'
+//dotenv.config()
 
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.CLOUD_API_KEY, 
-    api_secret: process.env.CLOUD_API_SECRET 
-  });
+//cloudinary.config({ 
+    //cloud_name: process.env.CLOUD_NAME, 
+    //api_key: process.env.CLOUD_API_KEY, 
+    //api_secret: process.env.CLOUD_API_SECRET 
+//});
 
-const uploadImageToCloudinary = (imageFile) => {
+//const uploadImageToCloudinary = (imageFile) => {
 
-   return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(imageFile.tempFilePath, (error, result) => {
-        if(error){
-            reject(error)
-            console.log(error)
-        } else {
-            resolve(result.secure_url)
-        }
-    })
-   })
-}
+  // return new Promise((resolve, reject) => {
+    //cloudinary.uploader.upload(imageFile.tempFilePath, (error, result) => {
+        //if(error){
+           // reject(error)
+           // console.log(error)
+       // } else {
+          //  resolve(result.secure_url)
+        //}
+  //  })
+  // })
+//}
 
 
-export default uploadImageToCloudinary
+//export default uploadImageToCloudinary
